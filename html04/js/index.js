b9983cc8e03a22ac43e4e72de897c76694705994
SoraPark <ksttlov@naver.com>
2021-09-01 09:19:12 +0900

    Feat: index 페이지 완성

---
+++ html04/js/index.js
@@ -0,0 +1,10 @@
+const loveButton = document.querySelector('button.js-love');
+const adultButton = document.querySelector('button.js-adult');
+
+loveButton.addEventListener('click', () => {
+  location.href = './test.html';
+});
+
+adultButton.addEventListener('click', () => {
+  location.href = './test.html';
+});
